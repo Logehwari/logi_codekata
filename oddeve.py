0a215ce5bb531c03e9bce1c434bce77d7aee9b9e
Logehwari <47217787+Logehwari@users.noreply.github.com>
2019-06-01 12:18:49 +0530

    Update oddeve.py

--- oddeve.py
+++ oddeve.py
@@ -1,7 +1,7 @@
 num = int(input())
-if num % 2 == 0:
+if num < 0:
+	print("invalid")
+elif num % 2 == 0:
   print("Even")
-elif num % 2 == 1:
-  print("Odd")
 else:
-  print("invalid")
+  print("Odd")
