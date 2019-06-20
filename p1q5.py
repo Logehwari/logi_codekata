d1259a727c46e626e1ce74568e3a9994eda17aeb
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-06-20 21:37:04 +0530

    Create p1q5.py

---
+++ p1q5.py
@@ -0,0 +1,5 @@
+ld1=input()
+ld2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for j in range(21):
+  if(ld1==ld2[j]):
+    print(j)
