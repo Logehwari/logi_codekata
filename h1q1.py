08f0ae4be06308016f96e0851bf6873602c8d465
Logeshwari <47217787+Logehwari@users.noreply.github.com>
2019-06-24 19:55:12 +0530

    Create h1q1.py

---
+++ h1q1.py
@@ -0,0 +1,15 @@
+ld1=int(input())
+n1=list(map(int,input().split()))
+tj=[]
+for x in range(ld1):
+    for i in range(x+1,len(n1)):
+        if(n1[i]==n1[x]):
+          tj.append(n1[x])
+
+if (len(tj)==0):
+    print("unique")
+else:
+    tj.sort()
+    tj1=set(tj)
+    for j in tj1:
+        print(j,end=" ")
